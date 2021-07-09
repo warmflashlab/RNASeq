f7fd264667efbfab04d5024d5b39d1b7d58b1a71
warmflasha <warmflasha@gmail.com>
2021-07-09 09:23:42 -0500

    Dataset new functionality, comments
    add ability to get only exact matches in Dataset.getExpressionByName

--- DataClasses/Dataset.m
+++ DataClasses/Dataset.m
@@ -5,12 +5,12 @@ classdef Dataset
         samples % MappedSample objects in an array, one per sample
         referenceDatabase % store link to the ref database
         conditionIds % id numbers for conditions, keeps track of replicates
-        conditionNames %names of conditions, one entry per id. 
+        conditionNames %names of conditions, one entry per id.
     end
     
     methods
         function obj = Dataset(samples,referenceDatabase,conditionIds,conditionNames)
-            %Dataset Construct an instance of this class
+            % Construct a new object of Dataset class
             obj.samples = samples;
             if nargin > 1
                 obj.referenceDatabase = referenceDatabase;
@@ -22,8 +22,8 @@ classdef Dataset
         end
         
         function datamatrix = getDataMatrix(obj)
-            %datamatrix export data from all samples as a matrix
-            %   Detailed explanation goes here
+            %getDataMatrix: export data from all samples as a matrix
+            
             datamatrix = zeros(length(obj.samples(1).data),length(obj.samples));
             for ii = 1:length(obj.samples)
                 datamatrix(:,ii) = obj.samples(ii).data;
@@ -31,6 +31,8 @@ classdef Dataset
         end
         
         function avgdatamatrix = getAverageDataMatrix(obj)
+            %getAverageDataMatrix: export data from all samples, average
+            %over replicates.
             dm = obj.getDataMatrix;
             cIds = obj.conditionIds;
             uIds = sort(unique(cIds));
@@ -44,24 +46,41 @@ classdef Dataset
         end
         
         function conds = getConditions(obj)
+            %returns the conditions of the samples. Will include all
+            %replicates separately. For unique conditions, use property
+            %conditionNames
             conds = {obj.samples.name};
         end
         
-        function getExpressionByName(obj,namestr,avgOverReplicates)
-            
+        function [outdata, geneNames]=getExpressionByName(obj,namestr,avgOverReplicates,onlyExact)
+            % getExpressionByName: returns expression of a particular
+            % gene/genes by name. 
+            % optional arguments: 
+            % avgOverReplicates - true to average over replicates (default
+            % true)
+            % onlyExact - true for only exact matches (default false)
             if ~exist('avgOverReplicates','var')
                 avgOverReplicates = true;
             end
             
+            if ~exist('onlyExact','var')
+                onlyExact = false;
+            end
+            
             gene_names = obj.samples(1).gene_names;
-            inds = find(contains(gene_names,namestr));
+            if onlyExact
+                inds = find(matches(gene_names,namestr));
+            else
+                inds = find(contains(gene_names,namestr));
+            end
             if avgOverReplicates
                 dat = obj.getAverageDataMatrix;
             else
                 dat = obj.getDataMatrix;
             end
             nconds = size(dat,2);
-            
+            outdata = dat(inds,:);
+            geneNames = gene_names(inds);
             for ii = 1:length(inds)
                 fprintf('%s\t',gene_names{inds(ii)});
                 for jj = 1:nconds
@@ -69,9 +88,7 @@ classdef Dataset
                 end
                 fprintf('\n');
             end
-        end
-        %new comment
-        
+        end        
         
     end
 end
